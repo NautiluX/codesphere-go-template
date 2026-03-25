aa2dc720dcbf61f9bf2d779ab69c8a54b7c5151f
Manuel Dewald <manuel.dewald@codesphere.com>
2026-03-25 14:49:27 +0100

    cs up commit

--- hello_templ.go
+++ hello_templ.go
@@ -5,8 +5,10 @@ package main
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
-import "github.com/a-h/templ"
-import templruntime "github.com/a-h/templ/runtime"
+import (
+	"github.com/a-h/templ"
+	templruntime "github.com/a-h/templ/runtime"
+)
 
 func hello(name string, calculationResult int) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
@@ -29,7 +31,7 @@ func hello(name string, calculationResult int) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<html><head><title>Hello ")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<html><head><title>Hello Alex ")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
